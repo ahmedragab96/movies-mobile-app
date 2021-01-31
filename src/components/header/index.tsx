e96d1a91a90ce9fbe6373422c2d3e094d81019a8
ahmedragab96 <hamadar1996@gmail.com>
2021-01-31 03:01:35 +0200

    #0 - Implement header component

---
+++ src/components/header/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import {
+  ImageBackground,
+  View,
+} from 'react-native';
+import {
+  useStyles,
+} from 'elephanz-rn-ui';
+import {
+  HeaderComponentProps,
+} from './types';
+import {
+  styles,
+} from './styles';
+import LinearGradient from 'react-native-linear-gradient';
+
+export const HeaderComponent: React.FC<HeaderComponentProps> = (props) => {
+  const {
+    imageSource,
+    colors,
+    customStyles,
+    children,
+  } = props;
+  const {
+    selectStyle,
+  } = useStyles(styles, customStyles);
+  return (
+    <View
+      style={selectStyle('container')}
+    >
+      <ImageBackground
+        style={selectStyle('imageBackground')}
+        source={imageSource}
+      >
+        <LinearGradient
+          style={selectStyle('linearGradient')}
+          colors={colors}
+        >
+          {children}
+        </LinearGradient>
+      </ImageBackground>
+    </View>
+  );
+}
+
+
+export * from './types';
