4ee39bfae511a560be05ccd1b584014de4a8d6b1
ahmedragab96 <hamadar1996@gmail.com>
2021-01-31 04:30:30 +0200

    #0 - add Item Details

--- src/components/movieCard/index.tsx
+++ src/components/movieCard/index.tsx
@@ -6,15 +6,19 @@ import {
 import {
   useStyles,
   Typography,
+  useTheme,
 } from 'elephanz-rn-ui';
 import {
   MovieCardComponentProps, MovieCardVariants
 } from './types';
 import {
   styles,
-} from './styles'
+} from './styles';
+import { Rating, AirbnbRating } from 'react-native-ratings';
+
 
 const MovieCardComponent: React.FC<MovieCardComponentProps> = (props) => {
+  const { theme } = useTheme();
   const { data, variant } = props;
   const {
     selectStyle,
@@ -54,6 +58,37 @@ const MovieCardComponent: React.FC<MovieCardComponentProps> = (props) => {
               uri: `https://image.tmdb.org/t/p/w780/${data.backdrop_path}`,
             }}
           />
+          <View style={selectStyle('ratingContainer')}>
+            {
+              data.adult && (
+                <Typography>
+                  {'16+'}
+                </Typography>
+              )
+            }
+            <Typography>
+              {'10+'}
+            </Typography>
+          </View>
+        </View>
+      )}
+      { variant === MovieCardVariants.DETAILS && (
+        <View style={selectStyle('cardContainer')}>
+          <Image
+            style={selectStyle('imageContainer')}
+            source={{
+              uri: `https://image.tmdb.org/t/p/w780/${data.poster_path}`,
+            }}
+          />
+          <View style={selectStyle('widthTypo')}>
+            <Typography
+              variant="button"
+              ellipsizeMode="tail"
+              numberOfLines={2}
+            >
+              {data.title}
+            </Typography>
+          </View>
         </View>
       )}
     </View>
